45959e5172e7576f9e879bd77643ff7653ae021c
Skwerl23 <cottermannelson@gmail.com>
2020-06-29 22:42:36 -0400

    version 0.3 update
    Added cases/deaths per day

--- chartbuilder.py
+++ chartbuilder.py
@@ -147,7 +147,6 @@ def deathChart():
     fig.add_trace(px.line(chartUSAJHU, x="Date", y="John Hopkins Total Deaths", title="John Hopkins Total Deaths", line_group="Location", color="Location", color_discrete_map={"United States": "green"}).data[0])
     # updates the look of the graph
     fig.update_layout(
-    #    updatemenus=list([dict(buttons=list_updatemenus)]), # doesn't work, misaligns all the states
         height=600,
         autosize=True,
         margin=dict(t=0, b=0, l=0, r=0),
@@ -242,7 +241,7 @@ def casesChart():
     states = sorted(states)
 
     # Build initial plot line from John Hopkins Data
-    fig = px.line(chartJHU, x='Date', y = "John Hopkins Total Cases", title='Deaths by Location', line_group="Location", color="Location")
+    fig = px.line(chartJHU, x='Date', y = "John Hopkins Total Cases", title='Cases by Location', line_group="Location", color="Location")
 
     #Build and add plot line for covidtracking to graph
     for state in states:
@@ -252,7 +251,7 @@ def casesChart():
         except: continue
     fig.add_trace(px.line(chartCTU, x="dateChecked", y='covidtracking.com Total Cases', title="covidtracking.com Total Cases", line_group="Location", color="Location", color_discrete_map={"United States": "black"}).data[0])
 
-    # Plot sEntire USA John Hopkins Numbers onto graph
+    # Plots Entire USA John Hopkins Numbers onto graph
     chartUSAJHU = chartJHU
     chartUSAJHU["John Hopkins Total Cases"] = chartUSAJHU.groupby('Date')["John Hopkins Total Cases"].transform('sum') # Combines USA
     chartUSAJHU = chartUSAJHU.loc[chartUSAJHU["Location"] == "Alabama"] # Combines USA
@@ -260,7 +259,6 @@ def casesChart():
     fig.add_trace(px.line(chartUSAJHU, x="Date", y="John Hopkins Total Cases", title="John Hopkins Total Cases", line_group="Location", color="Location", color_discrete_map={"United States": "green"}).data[0])
     # updates the look of the graph
     fig.update_layout(
-    #    updatemenus=list([dict(buttons=list_updatemenus)]), # doesn't work, misaligns all the states
         height=600,
         autosize=True,
 
@@ -272,3 +270,287 @@ def casesChart():
 
     )
     return fig
+
+
+def deathDayChart():
+    # Gather Paths to local Data Sources
+    jhuFolder = "c:\\temp\\covid19\\jhu\\"
+    CDCExcessFolder = "c:\\temp\\covid19\\CDCExcess\\"
+    covidTrackingFolder = "c:\\temp\\covid19\\covidtracking\\"
+
+    # Build John Hopkins Chart
+    chartJHU = False
+    chartJHU = pd.concat(pd.read_csv(jhuFolder + csv) for csv in os.listdir(jhuFolder))
+    chartJHU = chartJHU.rename(columns={'Province_State': 'Location', "Last_Update": "Date", "Deaths": "John Hopkins Deaths Per Day"})
+    chartJHU = chartJHU[chartJHU["Location"] != "Recovered" ]
+
+    # Build CDC Chart
+    chartCDCExcess = pd.read_csv(CDCExcessFolder + "CDCExcess.csv")
+    chartCDCExcess = chartCDCExcess.rename(columns={"State": "Location"})
+
+    # Build covidtracking.com Charts
+    chartCTS = pd.read_csv(covidTrackingFolder + "covidtrackingstates.csv")
+    chartCTS = chartCTS.rename(columns={'state': 'Location', "deathIncrease": "covidtracking.com Deaths Per Day"})
+    chartCTS = chartCTS.replace('AK', 'Alaska')
+    chartCTS = chartCTS.replace('AL', 'Alabama')
+    chartCTS = chartCTS.replace('AR', 'Arkansas')
+    chartCTS = chartCTS.replace('AS', 'American Samoa')
+    chartCTS = chartCTS.replace('AZ', 'Arizona')
+    chartCTS = chartCTS.replace('CA', 'California')
+    chartCTS = chartCTS.replace('CO', 'Colorado')
+    chartCTS = chartCTS.replace('CT', 'Connecticut')
+    chartCTS = chartCTS.replace('DC', 'District of Columbia')
+    chartCTS = chartCTS.replace('DE', 'Delaware')
+    chartCTS = chartCTS.replace('FL', 'Florida')
+    chartCTS = chartCTS.replace('GA', 'Georgia')
+    chartCTS = chartCTS.replace('GU', 'Guam')
+    chartCTS = chartCTS.replace('HI', 'Hawaii')
+    chartCTS = chartCTS.replace('IA', 'Iowa')
+    chartCTS = chartCTS.replace('ID', 'Idaho')
+    chartCTS = chartCTS.replace('IL', 'Illinois')
+    chartCTS = chartCTS.replace('IN', 'Indiana')
+    chartCTS = chartCTS.replace('KS', 'Kansas')
+    chartCTS = chartCTS.replace('KY', 'Kentucky')
+    chartCTS = chartCTS.replace('LA', 'Louisiana')
+    chartCTS = chartCTS.replace('MA', 'Maine')
+    chartCTS = chartCTS.replace('MD', 'Maryland')
+    chartCTS = chartCTS.replace('ME', 'Massachusetts')
+    chartCTS = chartCTS.replace('MI', 'Michigan')
+    chartCTS = chartCTS.replace('MN', 'Minnesota')
+    chartCTS = chartCTS.replace('MO', 'Missouri')
+    chartCTS = chartCTS.replace('MP', 'Northern Mariana Islands')
+    chartCTS = chartCTS.replace('MS', 'Mississippi')
+    chartCTS = chartCTS.replace('MT', 'Montana')
+    chartCTS = chartCTS.replace('NC', 'North Carolina')
+    chartCTS = chartCTS.replace('ND', 'North Dakota')
+    chartCTS = chartCTS.replace('NE', 'Nebraska')
+    chartCTS = chartCTS.replace('NH', 'New Hampshire')
+    chartCTS = chartCTS.replace('NJ', 'New Jersey')
+    chartCTS = chartCTS.replace('NM', 'New Mexico')
+    chartCTS = chartCTS.replace('NV', 'Nevada')
+    chartCTS = chartCTS.replace('NY', 'New York')
+    chartCTS = chartCTS.replace('OH', 'Ohio')
+    chartCTS = chartCTS.replace('OK', 'Oklahoma')
+    chartCTS = chartCTS.replace('OR', 'Oregon')
+    chartCTS = chartCTS.replace('PA', 'Pennsylvania')
+    chartCTS = chartCTS.replace('PR', 'Puerto Rico')
+    chartCTS = chartCTS.replace('RI', 'Rhode Island')
+    chartCTS = chartCTS.replace('SC', 'South Carolina')
+    chartCTS = chartCTS.replace('SD', 'South Dakota')
+    chartCTS = chartCTS.replace('TN', 'Tennessee')
+    chartCTS = chartCTS.replace('TX', 'Texas')
+    chartCTS = chartCTS.replace('UT', 'Utah')
+    chartCTS = chartCTS.replace('VA', 'Virginia')
+    chartCTS = chartCTS.replace('VI', 'Virgin Islands')
+    chartCTS = chartCTS.replace('VT', 'Vermont')
+    chartCTS = chartCTS.replace('WA', 'Washington')
+    chartCTS = chartCTS.replace('WI', 'Wisconsin')
+    chartCTS = chartCTS.replace('WV', 'West Virginia')
+    chartCTS = chartCTS.replace('WY', 'Wyoming')
+
+    chartCTU = pd.read_csv(covidTrackingFolder + "covidtrackingusa.csv")
+    chartCTU = chartCTU.rename(columns={'state': 'Location', "deathIncrease": "covidtracking.com Deaths Per Day"})
+    chartCTU["Location"] = "United States"
+    chartCTU = chartCTU.loc[chartCTU["dateChecked"].str.contains('^2020-03|^2020-04|^2020-05|^2020-06|^2020-07')]
+
+    #Build State list based on John Hopkins List - As it's a giant spreadsheet
+    states = set(chartJHU['Location'])
+    states = list(states)
+    states = states + ["United States"]
+    states = sorted(states)
+
+    # Build initial plot line from John Hopkins Data
+    fig = px.line()
+
+    for state in states:
+        try:
+            chartJHUTemp = chartJHU.loc[chartJHU["Location"] == state]
+            chartJHUTemp['John Hopkins Deaths Per Day'] = chartJHUTemp['John Hopkins Deaths Per Day'].diff()
+            fig.add_trace(px.line(chartJHUTemp, x='Date', y = "John Hopkins Deaths Per Day", title='Deaths by Location', line_group="Location", color="Location",
+                                  color_discrete_map={state: "green"}).data[0])
+        except: continue
+
+    # Customize Lists into smaller chunks for plotting - Specifically Higher CDC Estimates
+    chartCDCHigher = chartCDCExcess.loc[chartCDCExcess["Type"] == "Predicted (weighted)"]
+    chartCDCHigher = chartCDCHigher.loc[chartCDCHigher["Outcome"] == "All causes"]
+    chartCDCHigher = chartCDCHigher.loc[chartCDCHigher["Week Ending Date"].str.contains('^2020-03|^2020-04|^2020-05|^2020-06|^2020-07')]
+    chartCDCHigher = chartCDCHigher.rename(columns={'Excess Higher Estimate': 'CDC Excess Higher Estimate (week avg)', "Week Ending Date": "Date"})
+
+    # New york was divided into city/state, so this brings them back together
+    chartCDCHigherNY = chartCDCHigher.loc[chartCDCHigher["Location"].str.contains("New York")] # Corrects New York
+    chartCDCHigherNY['CDC Excess Higher Estimate (week avg)'] = chartCDCHigherNY.groupby('Date')['CDC Excess Higher Estimate (week avg)'].transform('sum') # Corrects New York
+    chartCDCHigher = chartCDCHigher.replace("New York", "This Doesn't Exist") # Corrects New York
+
+    # Customize Lists into smaller chunks for plotting - Specifically Lower CDC Estimate (week avg)s
+    chartCDCLower = chartCDCExcess.loc[chartCDCExcess["Type"] == "Predicted (weighted)"]
+    chartCDCLower = chartCDCLower.loc[chartCDCLower["Outcome"] == "All causes"]
+    chartCDCLower = chartCDCLower.loc[chartCDCLower["Week Ending Date"].str.contains('^2020-03|^2020-04|^2020-05|^2020-06|^2020-07')]
+    chartCDCLower = chartCDCLower.rename(columns={'Excess Lower Estimate': 'CDC Excess Lower Estimate (week avg)', "Week Ending Date": "Date"})
+
+    # New york was divided into city/state, so this brings them back together
+    chartCDCLowerNY = chartCDCLower.loc[chartCDCLower["Location"].str.contains("New York")] # Corrects New York
+    chartCDCLowerNY['CDC Excess Lower Estimate (week avg)'] = chartCDCLowerNY.groupby('Date')['CDC Excess Lower Estimate (week avg)'].transform('sum') # Corrects New York
+    chartCDCLower = chartCDCLower.replace("New York", "This Doesn't Exist") # Corrects New York
+
+    for state in states:
+        try:
+            chartTempHigher = chartCDCHigher.loc[chartCDCHigher["Location"] == state]
+            chartTempLower = chartCDCLower.loc[chartCDCLower["Location"] == state]
+            chartTempHigher['CDC Excess Higher Estimate (week avg)'] = chartTempHigher['CDC Excess Higher Estimate (week avg)'].floordiv(7)
+            chartTempLower['CDC Excess Lower Estimate (week avg)'] = chartTempLower['CDC Excess Lower Estimate (week avg)'].floordiv(7)
+            fig.add_trace(px.line(chartTempHigher, x="Date", y='CDC Excess Higher Estimate (week avg)', title="Excess Deaths Upper Limit", line_group="Location", color="Location", color_discrete_map={state: "red"}).data[0])
+            fig.add_trace(px.line(chartTempLower, x="Date", y='CDC Excess Lower Estimate (week avg)', title="Excess Deaths Lower Limit", line_group="Location", color="Location", color_discrete_map={state: "blue"}).data[0])
+        except: continue
+
+    #Build and add plot line for covidtracking to graph
+    for state in states:
+        try:
+            chartCTStemp = chartCTS.loc[chartCTS["Location"] == state]
+            fig.add_trace(px.line(chartCTStemp, x="dateModified", y='covidtracking.com Deaths Per Day', title="covidtracking.com Deaths Per Day", line_group="Location", color="Location", color_discrete_map={state: "black"}).data[0])
+        except: continue
+    fig.add_trace(px.line(chartCTU, x="dateChecked", y='covidtracking.com Deaths Per Day', title="covidtracking.com Deaths Per Day", line_group="Location", color="Location", color_discrete_map={"United States": "black"}).data[0])
+
+    # Plot sEntire USA John Hopkins Numbers onto graph
+    chartUSAJHU = chartJHU
+    chartUSAJHU["John Hopkins Deaths Per Day"] = chartUSAJHU.groupby('Date')["John Hopkins Deaths Per Day"].transform('sum') # Combines USA
+    chartUSAJHU = chartUSAJHU.loc[chartUSAJHU["Location"] == "Alabama"] # Combines USA
+    chartUSAJHU['John Hopkins Deaths Per Day'] = chartUSAJHU['John Hopkins Deaths Per Day'].diff()
+    chartUSAJHU = chartUSAJHU.replace("Alabama", "United States") # Combines USA
+    fig.add_trace(px.line(chartUSAJHU, x="Date", y="John Hopkins Deaths Per Day", title="John Hopkins Deaths Per Day", line_group="Location", color="Location", color_discrete_map={"United States": "green"}).data[0])
+    # updates the look of the graph
+    fig.update_layout(
+        height=600,
+        autosize=True,
+        margin=dict(t=0, b=0, l=0, r=0),
+        template="plotly_white",
+        hovermode="x",
+        yaxis_title="Deaths Per Day",
+        xaxis_title="Date"
+
+    )
+    return fig
+
+def casesDayChart():
+    # Gather Paths to local Data Sources
+    jhuFolder = "c:\\temp\\covid19\\jhu\\"
+    CDCExcessFolder = "c:\\temp\\covid19\\CDCExcess\\"
+    covidTrackingFolder = "c:\\temp\\covid19\\covidtracking\\"
+
+    # Build John Hopkins Chart
+    chartJHU = False
+    chartJHU = pd.concat(pd.read_csv(jhuFolder + csv) for csv in os.listdir(jhuFolder))
+    chartJHU = chartJHU.rename(columns={'Province_State': 'Location', "Last_Update": "Date", "Confirmed": "John Hopkins Cases Per Day"})
+    chartJHU = chartJHU[chartJHU["Location"] != "Recovered" ]
+
+    # Build covidtracking.com Charts
+    chartCTS = pd.read_csv(covidTrackingFolder + "covidtrackingstates.csv")
+    chartCTS = chartCTS.rename(columns={'state': 'Location', "positiveIncrease": "covidtracking.com Cases Per Day"})
+    chartCTS = chartCTS.replace('AK', 'Alaska')
+    chartCTS = chartCTS.replace('AL', 'Alabama')
+    chartCTS = chartCTS.replace('AR', 'Arkansas')
+    chartCTS = chartCTS.replace('AS', 'American Samoa')
+    chartCTS = chartCTS.replace('AZ', 'Arizona')
+    chartCTS = chartCTS.replace('CA', 'California')
+    chartCTS = chartCTS.replace('CO', 'Colorado')
+    chartCTS = chartCTS.replace('CT', 'Connecticut')
+    chartCTS = chartCTS.replace('DC', 'District of Columbia')
+    chartCTS = chartCTS.replace('DE', 'Delaware')
+    chartCTS = chartCTS.replace('FL', 'Florida')
+    chartCTS = chartCTS.replace('GA', 'Georgia')
+    chartCTS = chartCTS.replace('GU', 'Guam')
+    chartCTS = chartCTS.replace('HI', 'Hawaii')
+    chartCTS = chartCTS.replace('IA', 'Iowa')
+    chartCTS = chartCTS.replace('ID', 'Idaho')
+    chartCTS = chartCTS.replace('IL', 'Illinois')
+    chartCTS = chartCTS.replace('IN', 'Indiana')
+    chartCTS = chartCTS.replace('KS', 'Kansas')
+    chartCTS = chartCTS.replace('KY', 'Kentucky')
+    chartCTS = chartCTS.replace('LA', 'Louisiana')
+    chartCTS = chartCTS.replace('MA', 'Maine')
+    chartCTS = chartCTS.replace('MD', 'Maryland')
+    chartCTS = chartCTS.replace('ME', 'Massachusetts')
+    chartCTS = chartCTS.replace('MI', 'Michigan')
+    chartCTS = chartCTS.replace('MN', 'Minnesota')
+    chartCTS = chartCTS.replace('MO', 'Missouri')
+    chartCTS = chartCTS.replace('MP', 'Northern Mariana Islands')
+    chartCTS = chartCTS.replace('MS', 'Mississippi')
+    chartCTS = chartCTS.replace('MT', 'Montana')
+    chartCTS = chartCTS.replace('NC', 'North Carolina')
+    chartCTS = chartCTS.replace('ND', 'North Dakota')
+    chartCTS = chartCTS.replace('NE', 'Nebraska')
+    chartCTS = chartCTS.replace('NH', 'New Hampshire')
+    chartCTS = chartCTS.replace('NJ', 'New Jersey')
+    chartCTS = chartCTS.replace('NM', 'New Mexico')
+    chartCTS = chartCTS.replace('NV', 'Nevada')
+    chartCTS = chartCTS.replace('NY', 'New York')
+    chartCTS = chartCTS.replace('OH', 'Ohio')
+    chartCTS = chartCTS.replace('OK', 'Oklahoma')
+    chartCTS = chartCTS.replace('OR', 'Oregon')
+    chartCTS = chartCTS.replace('PA', 'Pennsylvania')
+    chartCTS = chartCTS.replace('PR', 'Puerto Rico')
+    chartCTS = chartCTS.replace('RI', 'Rhode Island')
+    chartCTS = chartCTS.replace('SC', 'South Carolina')
+    chartCTS = chartCTS.replace('SD', 'South Dakota')
+    chartCTS = chartCTS.replace('TN', 'Tennessee')
+    chartCTS = chartCTS.replace('TX', 'Texas')
+    chartCTS = chartCTS.replace('UT', 'Utah')
+    chartCTS = chartCTS.replace('VA', 'Virginia')
+    chartCTS = chartCTS.replace('VI', 'Virgin Islands')
+    chartCTS = chartCTS.replace('VT', 'Vermont')
+    chartCTS = chartCTS.replace('WA', 'Washington')
+    chartCTS = chartCTS.replace('WI', 'Wisconsin')
+    chartCTS = chartCTS.replace('WV', 'West Virginia')
+    chartCTS = chartCTS.replace('WY', 'Wyoming')
+
+    chartCTU = pd.read_csv(covidTrackingFolder + "covidtrackingusa.csv")
+    chartCTU = chartCTU.rename(columns={'state': 'Location', "positiveIncrease": "covidtracking.com Cases Per Day"})
+    chartCTU["Location"] = "United States"
+    chartCTU = chartCTU.loc[chartCTU["dateChecked"].str.contains('^2020-03|^2020-04|^2020-05|^2020-06|^2020-07')]
+
+    #Build State list based on John Hopkins List - As it's a giant spreadsheet
+    states = set(chartJHU['Location'])
+    states = list(states)
+    states = states + ["United States"]
+    states = sorted(states)
+
+    # Build initial plot line from John Hopkins Data
+    fig = px.line()
+
+    for state in states:
+        try:
+            chartJHUTemp = chartJHU.loc[chartJHU["Location"] == state]
+            chartJHUTemp['John Hopkins Cases Per Day'] = chartJHUTemp['John Hopkins Cases Per Day'].diff()
+            fig.add_trace(px.line(chartJHUTemp, x='Date', y = "John Hopkins Cases Per Day", title='Cases by Location', line_group="Location", color="Location",
+                                  color_discrete_map={state: "green"}).data[0])
+        except: continue
+    #Build and add plot line for covidtracking to graph
+
+    for state in states:
+        try:
+            chartCTStemp = chartCTS.loc[chartCTS["Location"] == state]
+            fig.add_trace(px.line(chartCTStemp, x="dateModified", y='covidtracking.com Cases Per Day', title="covidtracking.com Cases Per Day", line_group="Location", color="Location", color_discrete_map={state: "black"}).data[0])
+        except: continue
+    fig.add_trace(px.line(chartCTU, x="dateChecked", y='covidtracking.com Cases Per Day', title="covidtracking.com Cases Per Day", line_group="Location", color="Location", color_discrete_map={"United States": "black"}).data[0])
+
+#    Plots Entire USA John Hopkins Numbers onto graph
+    chartUSAJHU = chartJHU
+    chartUSAJHU["John Hopkins Cases Per Day"] = chartUSAJHU.groupby('Date')["John Hopkins Cases Per Day"].transform('sum') # Combines USA
+    chartUSAJHU = chartUSAJHU.loc[chartUSAJHU["Location"] == "Alabama"] # Combines USA
+    chartUSAJHU['John Hopkins Cases Per Day'] = chartUSAJHU['John Hopkins Cases Per Day'].diff()
+    chartUSAJHU = chartUSAJHU.replace("Alabama", "United States") # Combines USA
+    fig.add_trace(px.line(chartUSAJHU, x="Date", y="John Hopkins Cases Per Day", title="John Hopkins Cases Per Day", line_group="Location", color="Location", color_discrete_map={"United States": "green"}).data[0])
+
+#    updates the look of the graph
+    fig.update_layout(
+        height=600,
+        autosize=True,
+
+        margin=dict(t=0, b=0, l=0, r=0),
+        template="plotly_white",
+        hovermode="x",
+        yaxis_title="Cases Per Day",
+        xaxis_title="Date"
+
+    )
+    return fig
+
--- main.py
+++ main.py
@@ -14,7 +14,8 @@ dUpdate.covidTracking()
 
 casesFig = cb.casesChart()
 deathFig = cb.deathChart()
-
+casesDayFig = cb.casesDayChart()
+deathDayFig = cb.deathDayChart()
 
 # Creates HTML file temp-plot and opens it
 app = dash.Dash()
@@ -22,27 +23,46 @@ app = dash.Dash()
 app.layout = html.Div([
     html.Div(html.H1(children="Covid-19 Comparison Charts:")),
     html.Div([html.H2("Covid 19 Total Cases:"),
-        html.Div(
-            dcc.Graph(id="Cases",
-                      figure=casesFig)
-            )
-    ], style={
+              html.Div(
+                  dcc.Graph(id="Cases",
+                            figure=casesFig)
+              )
+              ], style={
         'height': '600',
         'width': '50%',
         'float': 'left'
     }),
     html.Div([html.H2("Covid 19 Total Deaths:"),
-        html.Div(
-            dcc.Graph(id="Deaths",
-                      figure=deathFig)
-            )
-    ], style={
+              html.Div(
+                  dcc.Graph(id="Deaths",
+                            figure=deathFig)
+              )
+              ], style={
+        'height': '600',
+        'width': '50%',
+        'float': 'left'
+    }),
+    html.Div([html.H2("Covid 19 Cases Per Day:"),
+              html.Div(
+                  dcc.Graph(id="Cases Per Day",
+                            figure=casesDayFig)
+              )
+              ], style={
+        'height': '600',
+        'width': '50%',
+        'float': 'left'
+    }),
+    html.Div([html.H2("Covid 19 Deaths Per Day:"),
+              html.Div(
+                  dcc.Graph(id="Deaths Per Day",
+                            figure=deathDayFig)
+              )
+              ], style={
         'height': '600',
         'width': '50%',
         'float': 'left'
     }),
 ], style={
-    'height': '700',
     'width': '100%'
 })
 app.run_server()
